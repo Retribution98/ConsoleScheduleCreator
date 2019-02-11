c81f63a5754a1aaec21ddb8d41d685897c1e257c
Kirill Suvorov <suvorov.k.a@effective-group.ru>
2019-02-11 17:39:02 +0300

    Transition to .NetCore

---
+++ ConsoleApp1/Utils/ScheduleUtils.cs
@@ -0,0 +1,16 @@
+﻿using ScheduleApp.DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScheduleCreator.Utils
+{
+    static internal class ScheduleUtils
+    {
+        public static bool IsReady(this JobDto job, int time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
